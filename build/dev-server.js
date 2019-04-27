8ce548388104fb502ee94a7cdf2aa9d360cb2692
zhengmao <zhengmao0613@163.com>
2019-04-27 19:13:54 +0800

    Modify some files

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,12 +1,9 @@
 const Webpack = require("webpack")
-const WebpackDevServer = require("webpack-dev-server")
+const express = require("express")
 const webpackConfig = require("./webpack.dev.config")
 const serverConfig = require("./server.config")
 
 const compiler = Webpack(webpackConfig)
+const app = express()
 
-const server = new WebpackDevServer(compiler, serverConfig)
-
-server.listen(serverConfig.port, serverConfig.host, () => {
-  console.log("dev server start...")
-})
+console.log(app)
