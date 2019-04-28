b8b7ddf8fad1cf01eaf7abff62ec4fe0ce22265e
zhengmao <zhengmao0613@163.com>
2019-04-28 13:39:37 +0800

    Add utils and error

--- src/global/index.js
+++ src/global/index.js
@@ -1,6 +1,14 @@
 import Vue from "vue"
 
-// Global css
-import "normalize.css"
+import * as utils from "utils"
+import displayError from "error"
 
+// Global css or stylus
+import "normalize.css"
 import "style.styl"
+
+window._displayError = displayError
+window._utils = utils
+window._global = {
+  isWechatBrowser: /micromessenger/i.test(window.navigator.userAgent),
+}
